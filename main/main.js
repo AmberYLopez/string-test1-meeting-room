61324e4e1e503cf845acf9ac117b6824fa675cea
AmberYLopez <1163281758@qq.com>
2016-08-01 10:24:05 +0800

    string-test1

--- main/main.js
+++ main/main.js
@@ -79,7 +79,30 @@ function buildReceipt(promotedItems, {totalPayPrice, totalSaved}) {
 }
 
 function buildReceiptString(receipt) {
-  // TODO
+  let lines = ['***<没钱赚商店>收据***'];
+  for(let {name,count,unit,price,payPrice} of receipt.receiptItems){
+    let line = `名称：${name}，数量：${count}${unit}，单价：${price.toFixed(2)}(元)，小计：${payPrice.toFixed(2)}(元)`;
+    lines.push(line);
+  }
+  lines.push('----------------------');
+  lines.push(`总计：${receipt.totalPayPrice.toFixed(2)}(元)`);
+  lines.push(`节省：${receipt.totalSaved.toFixed(2)}(元)`);
+  lines.push('**********************');
+  let receiptString = lines.join('\n');
+  require('fs').writeFileSync('1.txt',receiptString);
+  return receiptString;
+
+  /*
+   `***<没钱赚商店>收据***
+   名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+   名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+   名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+   ----------------------
+   总计：58.50(元)
+   节省：7.50(元)
+   **********************`;
+
+   */
 }
 
 module.exports = {
